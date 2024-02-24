5eae47de2ed34912f816e510bfd4b537112016ed
NewbeCloud <github@newbecloud.com>
2024-02-24 12:18:56 +0800

    application configuration file

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+import secrets
+from dotenv import load_dotenv
+
+load_dotenv()
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or secrets.token_hex(32)
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')\
+        or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
